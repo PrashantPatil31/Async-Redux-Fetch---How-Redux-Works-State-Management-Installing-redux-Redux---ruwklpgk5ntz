866d812e54726e90e9ced20d4999b8bfd7e3c4d8
Prashant Patil <88845868+PrashantPatil31@users.noreply.github.com>
2023-02-06 12:35:56 +0530

    new

--- src/components/store.js
+++ src/components/store.js
@@ -1,19 +1,28 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const slice = createSlice({
-    name: 'hot-news',
-    initialState: {
-        articlesNum: 0,
-        articles : []
+  name: "hot-news",
+  initialState: {
+    articlesNum: 0,
+    articles: [],
+  },
+  reducers: {
+    set(state, actions) {
+      let temp = state.articles;
+      state.articles = temp.concat(actions.payload.articles);
+      state.articlesNum = actions.payload.num;
     },
-    reducers:{
-        
-    }
-})
+    setNum(state, actions) {
+      state.articlesNum = actions.payload.num;
+    },
+  },
+});
 
 const store = configureStore({
-    
-})
+  reducer: {
+    hotNews: slice.reducer,
+  },
+});
 
 export default store;
 
